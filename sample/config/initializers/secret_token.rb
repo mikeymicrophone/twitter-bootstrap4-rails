170b2e776bbf52af666fa0bffd9cfb584c3b41f8
Rahul Singh <rahul@akaruilabs.com>
2015-10-03 23:01:06 +0530

    created sample rails and tested bootstrap4

---
+++ sample/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sample::Application.config.secret_key_base = '6bc95e1161208eef3d9e92d4b8f89ec66b9595f73e45e042bfaf9506f4e252c9e09a9525e09ececc42ed0015303b491b3bd4a1965518362607477c6bfb962863'
